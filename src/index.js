10f677e902f7d4c0c13d0cc1b7985f2e15a80912
qsb3008 <qsb3008@163.com>
2019-04-07 10:17:14 +0800

    jQuery实现

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
-class Person {
-  constructor(name) {
-    this.name = name
+class jQuery {
+  constructor(selector) {
+    let slice = Array.prototype.slice
+    let dom = slice.call(document.querySelectorAll(selector))
+    let len = dom ? dom.length : 0
+    for (let i = 0; i < len; i++) {
+      this[i] = dom[i]
+    }
+    this.length = len
+    this.selector = selector || ''
   }
-  getName() {
-    return this.name
+  append(node) {
+
+  }
+  addClass(name) {
+
+  }
+  html(data) {
+
   }
 }
 
-let person1 = new Person('qiushunbin')
-alert(person1.getName())
+window.$ = function (selector) {
+  // 工厂模式
+  return new jQuery(selector)
+}
