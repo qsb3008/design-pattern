e0f44e379c455914ab14039e9ff2241c99f6e5ed
qsb3008 <qsb3008@163.com>
2019-04-07 11:41:54 +0800

    loadImg

--- src/index.js
+++ src/index.js
@@ -1,11 +1,25 @@
-class Person {
-  constructor(name) {
-    this.name = name
-  }
-  getName() {
-    return this.name
-  }
+function loadImg(src) {
+  let promise = new Promise(function (resolve, resject) {
+    let img = document.createElement('img')
+    img.onload = function () {
+      resolve(img)
+    }
+    img.onerror = function () {
+      resject('图片加载失败')
+    }
+    img.src= src
+  })
+  return promise
 }
 
-let person1 = new Person('qiushunbin')
-console.log(person1.getName())
+let src = 'https://img2.mukewang.com/5a9fc8070001a82402060220-160-160.jpg'
+let result = loadImg(src)
+
+result.then(function (img) {
+  document.body.appendChild(img)
+  console.log(`width: ${img.width}`)
+  return img
+}).catch(function (ex) {
+  console.log(ex, ' : ex')
+})
+
