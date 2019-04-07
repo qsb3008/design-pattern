1dda18780e61282f596d35482b803d3e623c2607
qsb3008 <qsb3008@163.com>
2019-04-07 10:33:21 +0800

    uml类图

--- src/index.js
+++ src/index.js
@@ -1,11 +1,31 @@
 class Person {
-  constructor(name) {
+  constructor(name, house) {
     this.name = name
+    this.house = house
   }
-  getName() {
-    return this.name
+  saySomething() {
+    console.log('I am person')
   }
 }
 
-let person1 = new Person('qiushunbin')
-alert(person1.getName())
+class House {
+  constructor(city){
+    this.city = city
+  }
+  saySomething() {
+    console.log('I am from ' + city)
+  }
+}
+
+class A extends Person {
+  constructor(name, house) {
+    super(name, house)
+  }
+  saySomething() {
+    console.log('I am A;I am from ' + this.house.city)
+  }
+}
+
+let aHouse = new House('北京')
+let p = new A('zhangsan', aHouse)
+p.saySomething()
